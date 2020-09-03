6720859dd8694bd2e862b234b4095da8b33667a8
vinkeSch <kevin_manu@hotmail.com>
2020-09-03 16:21:44 +0200

    Main menu implementation

---
+++ app/src/main/java/com/example/scorekeeper/BluetoothService.kt
@@ -0,0 +1,112 @@
+package com.example.scorekeeper
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.os.ParcelUuid
+import android.util.Log
+import java.io.IOException
+import java.util.*
+
+/**
+ * This class does all the work for setting up and managing Bluetooth
+ * connections with other devices. It has a thread that listens for
+ * incoming connections, a thread for connecting with a device, and a
+ * thread for performing data transmissions when connected.
+ */
+
+class BluetoothService(device: BluetoothDevice) {
+
+    // Debugging
+    private val TAG = "BluetoothService"
+
+    // Name for the SDP record when creating server socket
+    private val NAME = "BluetoothSecure"
+
+    // El identificador único universal (UUID) es un ID de string con un formato estandarizado de
+    // 128 bits que se emplea para identificar información de manera exclusiva. Conforma la base
+    // del acuerdo de conexión con el dispositivo del cliente. Es decir, cuando el cliente
+    // intenta conectarse con este dispositivo, incluye un UUID que identifica de manera única el
+    // servicio con el cual desea conectarse.
+
+    // Unique UUID for this application
+    private var MY_UUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66")
+
+    private val bluetoothAdapter: BluetoothAdapter? = null
+
+    private var mmDevice = device
+    private var mSecureAcceptThread: AcceptThread? = null
+
+    /**
+     * Start the chat service. Specifically start AcceptThread to begin a
+     * session in listening (server) mode. Called by the Activity onResume()
+     */
+    @Synchronized
+    fun start() {
+        Log.d(TAG, "start()")
+
+        // Start the thread to listen on a BluetoothServerSocket
+        if (mSecureAcceptThread == null) {
+            mSecureAcceptThread = AcceptThread()
+            mSecureAcceptThread!!.start()
+        }
+    }
+
+    fun stop() {
+        Log.d(TAG, "stop()")
+
+        if (mSecureAcceptThread != null) {
+            mSecureAcceptThread!!.cancel()
+            mSecureAcceptThread = null
+        }
+    }
+
+    /**
+     * A diferencia del protocolo TCP/IP, RFCOMM solo permite un cliente conectado por canal
+     * a la vez, por lo que, en la mayoría de los casos, tiene sentido llamar a close() en el
+     * BluetoothServerSocket inmediatamente después de aceptar un socket conectado.
+     */
+    private inner class AcceptThread () : Thread() {
+
+        private val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            if(mmDevice.uuids != null) {
+                val list: Array<ParcelUuid> = mmDevice.uuids
+                MY_UUID = UUID.fromString(list[0].toString())
+            }
+            Log.i(TAG, "UUID used: $MY_UUID")
+            bluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord(NAME, MY_UUID)
+        }
+
+        override fun run() {
+            Log.i(TAG, "BEGIN mAcceptThread:")
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter?.cancelDiscovery()
+
+            // Keep listening until exception occurs or a socket is returned.
+            var shouldLoop = true
+            while (shouldLoop) {
+                val socket: BluetoothSocket? = try {
+                    mmServerSocket?.accept()
+                } catch (e: IOException) {
+                    Log.e(TAG, "Socket's accept() method failed", e)
+                    shouldLoop = false
+                    null
+                }
+                socket?.also {
+                    mmServerSocket?.close()
+                    shouldLoop = false
+                }
+            }
+        }
+
+        // Closes the connect socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmServerSocket?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the connect socket", e)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -1,19 +1,118 @@
 package com.example.scorekeeper
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.content.Intent
 import android.os.Bundle
+import android.os.ParcelUuid
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import java.io.IOException
+import java.util.*
 
 class MainActivity :  AppCompatActivity(), NavigationHost {
+
+    private val TAG = "MainActivity"
+    private val REQUEST_ENABLE_BT = 1
+
+    //Representa el adaptador local de Bluetooth (radio Bluetooth). El BluetoothAdapter es el punto
+    // de entrada de toda interacción de Bluetooth. Gracias a esto, puedes ver otros dispositivos
+    // Bluetooth, consultar una lista de los dispositivos conectados (sincronizados), crear una
+    // instancia de BluetoothDevice mediante una dirección MAC conocida y crear un
+    // BluetoothServerSocket para recibir comunicaciones de otros dispositivos.
+
+    // Local Bluetooth adapter
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+
+    // Member object for the bluetooth services
+    private var mBluetoothService: BluetoothService? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mBluetoothAdapter= BluetoothAdapter.getDefaultAdapter()
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.container, MatchFragment())
+                .add(R.id.container, MainMenuFragment())
                 .commit()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mBluetoothAdapter == null) {
+            return
+        }
+        // If BT is not on, request that it be enabled.
+        // setupConn() will then be called during onActivityResult
+        if (!mBluetoothAdapter!!.isEnabled) {
+            val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT)
+            // Otherwise, setup the connection session
+        } else if (mBluetoothService == null) {
+            setupConn()
+        }
+    }
+
+    private fun setupConn() {
+        Log.i(TAG, "setupConn")
+
+        // Para inicializar una conexión con el dispositivo remoto que acepta conexiones en un
+        // socket de servidor abierto, primero debes obtener un objeto BluetoothDevice que
+        // represente al dispositivo remoto. -> AB Shutter3: FF:FF:FF:FF:FF:FF
+        val device: BluetoothDevice? = mBluetoothAdapter?.getRemoteDevice("FF:FF:FF:FF:FF:FF")
+
+        // Initialize the BluetoothChatService to perform bluetooth connections
+        mBluetoothService = device?.let { BluetoothService(it) }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (mBluetoothAdapter?.isEnabled!!) {
+                    Toast.makeText(
+                        this, "Bluetooth enabled",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    /**
+                     * Antes de llevar a cabo la detección de dispositivos, es importante
+                     * consultar el conjunto de dispositivos sincronizados a fin de ver si
+                     * el dispositivo deseado ya es conocido. Para ello, llama a
+                     * getBondedDevices(). Esto devuelve un conjunto de objetos BluetoothDevice
+                     * que representa a los dispositivos sincronizados. Por ejemplo, puedes
+                     * consultar todos los dispositivos sincronizados y obtener el nombre y
+                     * la dirección MAC de cada uno, como se demuestra en el siguiente fragmento
+                     * de código:
+                     * Por ejemplo:
+                     * Galaxy Buds (19E1): F4:7D:EF:05:19:E1
+                     * REIVAX SPEAKER: 88:C6:26:1B:1B:1E
+                     * AB Shutter3: FF:FF:FF:FF:FF:FF
+                     */
+                    val pairedDevices: Set<BluetoothDevice>? = mBluetoothAdapter?.bondedDevices
+                    pairedDevices?.forEach { device ->
+                        println("DEVICE: ${device.name}: ${device.address}")
+                    }
+                } else {
+                    Toast.makeText(
+                        this, "Bluetooth disabled",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(
+                    this, "Bluetooth enabling has been canceled",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
@@ -35,5 +134,8 @@ class MainActivity :  AppCompatActivity(), NavigationHost {
         transaction.commit()
     }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        mBluetoothService?.stop()
+    }
 }
