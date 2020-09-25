8bae9aedd965072571723f09d9d7997f17d6fc18
vinkeSch <kevin_manu@hotmail.com>
2020-09-25 18:05:46 +0200

    Sign in implementation

--- app/src/main/java/com/example/scorekeeper/SplashActivity.kt
+++ app/src/main/java/com/example/scorekeeper/SplashActivity.kt
@@ -14,7 +14,7 @@ class SplashActivity : AppCompatActivity() {
 
         motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                startActivity(Intent(this@SplashActivity, SignInActivity::class.java))
             }
 
             override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) { }
